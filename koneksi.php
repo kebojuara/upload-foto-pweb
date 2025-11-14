3af552e876a13dad46f9e69d49d1531470c3c536
kebojuara <6329.william@gmail.com>
2025-11-14 08:29:11 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host = "sql209.infinityfree.com";
+$username = "if0_40413849";
+$password = "if0_40413849";
+$database = "if0_40413849_siswa";
+// Koneksi ke MySQL dengan PDO
+$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+?>
